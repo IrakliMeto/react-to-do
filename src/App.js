0c3d3696a48594c006ded93cfdff8a8898705cd3
Irakli metonidze <imetonidze@newage.io>
2023-04-09 09:28:32 +0400

    ..
    ..

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./reset.scss";
 import "./App.scss";
-import { Modal } from "./components/rate/modal";
-import { TipCalculator } from "./components/tipCalculator/TipCalculator";
-import { AccordionCard } from "./components/accordion/AccordionCard";
+
 import { Todo } from "./components/todo/Todo";
 
 function App() {
   return (
     <div className="App">
-      {/* <Modal /> */}
-
-      {/* <TipCalculator /> */}
-
-      {/* <AccordionCard /> */}
-
       <Todo />
     </div>
   );
